5b5ee6abede701bca1215a961038f40a0cd0bc89
victor <victor.freire.oliveira@gmail.com>
2023-08-11 23:20:32 -0300

    developing footer section

--- src/Footer/index.jsx
+++ src/Footer/index.jsx
@@ -1,10 +1,25 @@
 import styles from './Footer.module.css'
 
+import vfo from '../assets/vfo_white.svg'
+import linkedin from '../assets/linkedin.svg'
+import github from '../assets/github.svg'
+
 const Footer = () => {
     return (
         <footer className={styles.footer}>
             <div className={styles.container}>
-                Footer text
+                <img src={vfo} alt="VFO logo" />
+                <div className={styles.footerTxt}>
+                    &copy; Victor Freire Oliveira • Front-end developer | Multimedia Designer • All rights reserved
+                </div>
+                <div className="footerIcons">
+                    <a href="https://github.com/oliveira-victor" target="_blank" rel="noreferrer">
+                        <img src={linkedin} alt="LinkedIn icon" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/victor-fo/" target="_blank" rel="noreferrer">
+                        <img src={github} alt="GitHub icon" />
+                    </a>
+                </div>
             </div>
             <div className={styles.emptyMobileSpace}></div>
         </footer>
