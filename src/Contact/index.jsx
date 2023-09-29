35dc56a4b0ceb42d3cafe768bd8807dd9eda50e9
victor <victor.freire.oliveira@gmail.com>
2023-09-29 11:04:41 -0300

    centering div in contact section; add github icon

--- src/Contact/index.jsx
+++ src/Contact/index.jsx
@@ -3,6 +3,7 @@ import styles from './Contact.module.css'
 
 import linkedin from './assets/contact-linkedin.svg'
 import gmail from './assets/contact-gmail.svg'
+import github from '../assets/github.svg'
 
 const Contact = () => {
     return (
@@ -14,8 +15,9 @@ const Contact = () => {
                         Thank you for visiting my page! You can contact me on social media or just send me an email at: victor.freire.oliveira@gmail.com
                     </p>
                     <div className={styles.iconsContainer}>
-                        <a href="https://www.linkedin.com/in/victor-fo/"><img src={linkedin} alt="LinkedIn icon" /></a>
+                        <a href="https://www.linkedin.com/in/victor-fo/" target="_blank" rel="noreferrer"><img src={linkedin} alt="LinkedIn icon" /></a>
                         <a href="mailto:victor.freire.oliveira@gmail.com"><img src={gmail} alt="Gmail icon" /></a>
+                        <a href="https://github.com/oliveira-victor" target="_blank" rel="noreferrer"><img src={github} alt="Gmail icon" /></a>
                     </div>
                 </div>
                 <Footer />
