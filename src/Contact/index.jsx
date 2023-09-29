b0d461f7adfcc79df80c78123d427dd1790384b2
victor <victor.freire.oliveira@gmail.com>
2023-09-29 00:58:16 -0300

    css improvements, building contact section

--- src/Contact/index.jsx
+++ src/Contact/index.jsx
@@ -1,14 +1,26 @@
 import Footer from '../Footer'
 import styles from './Contact.module.css'
 
+import linkedin from './assets/contact-linkedin.svg'
+import gmail from './assets/contact-gmail.svg'
+
 const Contact = () => {
     return (
-        <section className={styles.contact}>
-            <div className={styles.container}>
+        <>
+            <section className={styles.contact}>
                 <h2>Contact</h2>
-            </div>
-            <Footer />
-        </section>
+                <div className={styles.container}>
+                    <p>
+                        Thank you for visiting my page! You can contact me on social media or just send me an email at: victor.freire.oliveira@gmail.com
+                    </p>
+                    <div className={styles.iconsContainer}>
+                        <a href="https://www.linkedin.com/in/victor-fo/"><img src={linkedin} alt="LinkedIn icon" /></a>
+                        <a href="mailto:victor.freire.oliveira@gmail.com"><img src={gmail} alt="Gmail icon" /></a>
+                    </div>
+                </div>
+                <Footer />
+            </section>
+        </>
     )
 }
 
