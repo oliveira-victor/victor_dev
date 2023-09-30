4bb87606e655e9c2373f898658d49c7dea6d3095
victor <victor.freire.oliveira@gmail.com>
2023-09-30 12:32:32 -0300

    building toggle buttons on services page

--- src/services/index.jsx
+++ src/services/index.jsx
@@ -1,9 +1,42 @@
+import { useState } from 'react'
 import styles from './Services.module.css'
 
 const Services = () => {
+
+    const [toggleWebdev, setToggleWebdev] = useState(true);
+    const [toggleDesign, setToggleDesign] = useState(false);
+
+    const handleToggleWebdev = () => {
+        setToggleDesign(false);
+        setToggleWebdev(true);
+    }
+
+    const handleToggleDesign = () => {
+        setToggleWebdev(false);
+        setToggleDesign(true);
+    }
+
     return (
         <section className={styles.services} id="services">
             <h2>Services</h2>
+            <div className={styles.servicesContainer}>
+                <div className={styles.buttonsWrapper}>
+                    <div className={toggleWebdev ? `${styles.serviceBtn_isSelected} ${styles.servicesBtn}` : `${styles.servicesBtn}`} onClick={handleToggleWebdev}>
+                        <h3 className={toggleWebdev ? `${styles.selectedBtn}` : ''}>Web development</h3>
+                    </div>
+                    <div className={toggleDesign ? `${styles.serviceBtn_isSelected} ${styles.servicesBtn}` : `${styles.servicesBtn}`} onClick={handleToggleDesign}>
+                        <h3 className={toggleDesign ? `${styles.selectedBtn}` : ''}>Multimedia design</h3>
+                    </div>
+                </div>
+                {toggleWebdev &&
+                    <div className={`${styles.DevCanvas} fadeIn`}>
+                        Web dev
+                    </div>}
+                {toggleDesign &&
+                    <div className={`${styles.designCanvas} fadeIn`}>
+                        Design
+                    </div>}
+            </div>
         </section>
     )
 }
