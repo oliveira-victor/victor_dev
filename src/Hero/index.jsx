a8b2f6e1df9cf95fbb1053810eae744f31f0bad3
victor <victor.freire.oliveira@gmail.com>
2023-10-04 23:18:54 -0300

    improving hero section with button

--- src/Hero/index.jsx
+++ src/Hero/index.jsx
@@ -1,14 +1,6 @@
 import { useRef, useEffect } from "react"
 import styles from "./Hero.module.css"
 
-import layer1 from "./assets/layer1.png"
-import layer3 from "./assets/layer3.webp"
-import layer4 from "./assets/layer4.webp"
-import layer5 from "./assets/layer5.webp"
-import layer6 from "./assets/layer6.webp"
-import layer7 from "./assets/layer7.webp"
-import layer8 from "./assets/layer8.webp"
-
 import vfo from '../assets/vfo_white.svg'
 import github from './assets/github.svg'
 import linkedin from './assets/linkedin.svg'
@@ -29,9 +21,14 @@ const Hero = () => {
         <>
             <section className={styles.hero} id="main">
                 <div className={styles.mainLayer}>
-                    <h1>Victor Freire Oliveira<br /><span className={styles.subtitle}>Front-End Developer</span></h1>
-                </div>
                 <img src={vfo} className={styles.vfoLogo} alt="VFO logo" />
+                    <h1>Victor Freire Oliveira
+                        <br /><span className={styles.subtitle}>Front-End Developer</span>
+                    </h1>
+                    <a href="#services">
+                        <button className={`${styles.startBtn} ${styles.btn1}`}>Get started</button>
+                    </a>
+                </div>
                 <div className={styles.socialMedia}>
                     <a href="https://github.com/oliveira-victor" target="_blank" rel="noreferrer">
                         <img src={github} alt="GitHub icon" title="GitHub" />
@@ -42,7 +39,9 @@ const Hero = () => {
                 </div>
                 <div className={styles.container}>
                     <div data-relative-input="true" id={styles.scene} ref={refParallax}>
-                        <div className={styles.layer} data-depth="0.5"></div>
+                        <div className={styles.layer} data-depth="0.2">
+                            {/* <h1 id={styles.layer2}>Victor Freire Oliveira<br /><span className={styles.subtitle}>Front-End Developer</span></h1> */}
+                        </div>
                     </div>
                 </div>
             </section>
