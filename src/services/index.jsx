f9b7610bee49ed066f7522ba6ecee7dfa909d60f
victor <victor.freire.oliveira@gmail.com>
2023-10-09 11:18:51 -0300

    updating design section

--- src/services/index.jsx
+++ src/services/index.jsx
@@ -186,7 +186,7 @@ const Services = () => {
                                     </div>
                                 </div>
                                 <p>
-                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur labore neque asperiores assumenda, provident adipisci ipsam modi tempora praesentium? Aut illo nostrum dignissimos sint vel consectetur quibusdam, ratione eius molestiae.
+                                    A great product should come with great visual content that catches the eyes of the users and makes it easy to use and understand the content. I make inspiring visual elements for diverse projects, working with Graphic Design and digital illustration.
                                 </p>
                             </div>
                         </div>
@@ -205,28 +205,28 @@ const Services = () => {
                             </div>
                             <p>Here are some examples on how you can count on me:</p>
                             <Accordion
-                                question="Design for interfaces"
-                                answer="Whether you need a personal or professional web site of any kind to show your content to the world, I can make you a high performance page that will engage the user with beautiful and interactive content, as well as a great care for the accessbility and easy way of browsing."
+                                question="User interface design"
+                                answer="This is a service for those who have an idea of a project that needs to interact with users, through a screen, with an intuitive and clean interface. I design user interfaces focusing on maximizing usability and user experience."
                             />
                             <Accordion
-                                question="Logo creation"
-                                answer="Do you need to promote a product, a course, an event or any other kind of service? This single and straight to the point kind of page will do it. This is a great and fast solution and you can expect practicality."
+                                question="Visual identity"
+                                answer="I design logo, web applications, animation, icons, and other elements that represent a company or a product, dealing with the overall asthetic representation of the brand."
                             />
                             <Accordion
                                 question="Digital art"
-                                answer="This is a platform, an information management tool that will display various types of visual data from a database in one place in an easy way to understand. A great tool for businesses."
+                                answer="Whenever you need some piece of art to make your ideas come true, digital art is one solution as it's highly editable, easy to transfer and to be applied into different projects and objects."
                             />
                             <Accordion
                                 question="Book illustration"
-                                answer="Need to develop a practical app that will solve problems or entertain? A web app is great for that, as it can be accessed anywhere on a device with a browser. If you need an app for automatizing tasks and solving problems, a fast and lightweight web app can help, and I can do that for you."
+                                answer="If you are a writer and want to make your stories come to life in a beautiful and inspiring way, you can count on my digital illustration work in different styles, depending on your intention."
                             />
                             <Accordion
                                 question="Minimalist animation"
-                                answer="If you need an online game, I can develop a low complexity and efficient one in JavaScript, whether you need it for educational purposes or just for pure entertainment, just bring the concept and I'll turn it into something playable and fun."
+                                answer="That's some kind of simple animation for games, applications, videos or any kind of visual content. The animations are made with simple movements that will efficiently make the illustration come to life."
                             />
                             <Accordion
-                                question="Illustration for custom products"
-                                answer="That's a great way to avoid those over used social links pages you see in people's bio on social media. You can have your own unique customized and interactive links page, that will represent you or your company in a professional way."
+                                question="Character design"
+                                answer="That's more than just drawing a character, it's about showing personality and giving identity to characters through visual elements and details. It's essential for many kinds of projects such as book stories and animation."
                             />
                             <a href='#services' onClick={handleToggleWebdev}>
                                 <button className={styles.changeServiceLink}>See also my frontend development service</button>
