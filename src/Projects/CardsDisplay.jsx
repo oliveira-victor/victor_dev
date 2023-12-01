3c4a99e7ba88dcd47e9e9855bf50637d12615104
oliveira-victor <victor.freire.oliveira@gmail.com>
2023-12-01 15:56:25 -0300

    feat: adding DigitalDice card

--- src/Projects/CardsDisplay.jsx
+++ src/Projects/CardsDisplay.jsx
@@ -11,6 +11,8 @@ import react from '../assets/logo-react.svg'
 import gulp from '../assets/logo-gulp.svg'
 import sass from '../assets/logo-sass.svg'
 import styledComponents from '../assets/logo-styledComponents.png'
+import redux from '../assets/ico-redux.svg'
+import typescript from '../assets/ico-typescript.svg'
 
 import arrow from '../assets/arrow.svg'
 
@@ -24,14 +26,14 @@ const CardsDisplay = () => {
         <>
             <div className={styles.mainCardsContainer}>
                 <Card
-                    link="https://vfostudio.vercel.app/"
-                    id="vfostudio"
-                    cardTitle="VFO Studio"
-                    techs1={html}
-                    techs2={javascript}
-                    techs3={sass}
-                    techs4={gulp}
-                    text={t('cardVFOstudio')}
+                    link="https://digitaldice.vercel.app/"
+                    id="digitaldice"
+                    cardTitle="Digital Dice"
+                    techs1={react}
+                    techs2={redux}
+                    techs3={typescript}
+                    techs4={styledComponents}
+                    text={t('digitalDice')}
                 />
                 <Card
                     link="https://theavatarmaker.vercel.app/"
@@ -52,16 +54,28 @@ const CardsDisplay = () => {
                     text={t('cardMamiMoon')}
                 />
                 <Card
-                    link="https://guessthenumber-game.vercel.app/"
-                    id="guessthenumber"
-                    cardTitle="Guess The Number"
+                    link="https://vfostudio.vercel.app/"
+                    id="vfostudio"
+                    cardTitle="VFO Studio"
                     techs1={html}
-                    techs2={css}
-                    techs3={javascript}
-                    text={t('cardNumbersGame')}
+                    techs2={javascript}
+                    techs3={sass}
+                    techs4={gulp}
+                    text={t('cardVFOstudio')}
                 />
                 {toggleExtraCards ?
                     <>
+                        <div className="fadeIn">
+                            <Card
+                                link="https://guessthenumber-game.vercel.app/"
+                                id="guessthenumber"
+                                cardTitle="Guess The Number"
+                                techs1={html}
+                                techs2={css}
+                                techs3={javascript}
+                                text={t('cardNumbersGame')}
+                            />
+                        </div>
                         <div className="fadeIn">
                             <Card
                                 link="https://vfo-parallax.vercel.app/"
@@ -81,15 +95,6 @@ const CardsDisplay = () => {
                 <div className={styles.loadMoreCards} onClick={() => setToggleExtraCards(true)}><span>{t('loadMoreCards')}</span> <img className="float" src={arrow} alt="Arrow down icon" /></div>
                 : ''
             }
-
-            {/* <Card 
-            link="https://vfostudio.vercel.app/"
-            id="card_img4" 
-            cardTitle="Bear Adventure" 
-            techs1={react}
-            techs2={styledComponents}
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu ipsum ac lacus egestas rutrum ac eu eros. Proin ac tempus tortor. Morbi quam turpis, lobortis nec diam id, porta condimentum ante. Ut elementum placerat consequat. Integer luctus ipsum et tellus luctus vehicula."
-        /> */}
             {/* <Card 
             link="https://fortunecookies.vercel.app/"
             id="card_img6" 
