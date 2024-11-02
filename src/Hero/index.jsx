81400ab981dbe4aafe2fdff59e57d8fe0ab1b567
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-11-02 16:45:48 -0300

    feat: hero techs icons

--- src/Hero/index.jsx
+++ src/Hero/index.jsx
@@ -1,10 +1,22 @@
+import { useTranslation } from "react-i18next"
+import LangSwitcher from "./LangSwitcher"
+
 import styles from "./Hero.module.css"
 
 import vfo from '../assets/vfo_white.svg'
 import github from './assets/github.svg'
 import linkedin from './assets/linkedin.svg'
-import LangSwitcher from "./LangSwitcher"
-import { useTranslation } from "react-i18next"
+
+import html from './assets/heroicon-html.svg'
+import css from './assets/heroicon-css.svg'
+import javascript from './assets/heroicon-javascript.svg'
+import typescrip from './assets/heroicon-typescript.svg'
+import react from './assets/heroicon-react.svg'
+import next from './assets/heroicon-next.svg'
+import redux from './assets/heroicon-redux.svg'
+import sass from './assets/heroicon-sass.svg'
+import gulp from './assets/heroicon-gulp.svg'
+import other from './assets/heroicon-other.svg'
 
 const Hero = () => {
 
@@ -18,6 +30,18 @@ const Hero = () => {
                     <h1 className="fadeIn">Victor Freire Oliveira
                         <br /><span className={`${styles.subtitle} fadeIn2`}>{t('VFOsubtitle')}</span>
                     </h1>
+                    <div className={`${styles.heroIcons} fadeIn3`}>
+                        <img src={html} alt="HTML icon" />
+                        <img src={css} alt="CSS icon" />
+                        <img src={javascript} alt="JavaScript icon" />
+                        <img src={typescrip} alt="TypeScript icon" />
+                        <img src={react} alt="React icon" />
+                        <img src={next} alt="Next icon" />
+                        <img src={redux} alt="Redux icon" />
+                        <img src={sass} alt="Sass icon" />
+                        <img src={gulp} alt="Gulp icon" />
+                        <img src={other} alt="Other icon" />
+                    </div>
                     <a href="#services">
                         <button className={`${styles.startBtn} ${styles.btn1}`}>{t('startBtn')}
                         </button>
