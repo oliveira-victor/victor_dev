d199c0775373b9cf34e8683c30cadb7bd1056900
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-11-07 13:34:15 -0300

    feat: animate contact icons

--- src/Contact/index.jsx
+++ src/Contact/index.jsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react"
 import { useTranslation } from 'react-i18next'
+
 import Footer from '../Footer'
 
 import styles from './Contact.module.css'
@@ -11,6 +13,28 @@ const Contact = () => {
 
     const { t } = useTranslation("contactSection")
 
+    const [pageAtBottom, setPageAtBottom] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+                onPageFullyScrolled()
+            } else {
+                setPageAtBottom(false)
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll)
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    }, [])
+
+    const onPageFullyScrolled = () => {
+        setPageAtBottom(true)
+    }
+
     return (
         <>
             <section className={styles.contact}>
@@ -20,9 +44,15 @@ const Contact = () => {
                         {t('contactInfo')}<br />dev.victor.fo@gmail.com
                     </p>
                     <div className={styles.iconsContainer}>
-                        <a href="https://www.linkedin.com/in/victor-fo/" target="_blank" rel="noreferrer"><img src={linkedin} alt="LinkedIn icon" /></a>
-                        <a href="mailto:dev.victor.fo@gmail.com"><img src={gmail} alt="Gmail icon" /></a>
-                        <a href="https://github.com/oliveira-victor" target="_blank" rel="noreferrer"><img src={github} alt="Gmail icon" /></a>
+                        <a href="https://www.linkedin.com/in/victor-fo/" target="_blank" rel="noreferrer">
+                            <img className={pageAtBottom ? styles.bounce1 : ''} src={linkedin} alt="LinkedIn icon" />
+                        </a>
+                        <a href="mailto:dev.victor.fo@gmail.com">
+                            <img className={pageAtBottom ? styles.bounce2 : ''} src={gmail} alt="Gmail icon" />
+                        </a>
+                        <a href="https://github.com/oliveira-victor" target="_blank" rel="noreferrer">
+                            <img className={pageAtBottom ? styles.bounce3 : ''} src={github} alt="Gmail icon" />
+                        </a>
                     </div>
                 </div>
                 <Footer />
