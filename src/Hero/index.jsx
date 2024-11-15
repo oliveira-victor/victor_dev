636976560ef7f5729af5399f077867f59dd6afcf
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-11-15 13:38:36 -0300

    feat: hero icons reveal effect

--- src/Hero/index.jsx
+++ src/Hero/index.jsx
@@ -17,30 +17,64 @@ import redux from './assets/heroicon-redux.svg'
 import sass from './assets/heroicon-sass.svg'
 import gulp from './assets/heroicon-gulp.svg'
 import other from './assets/heroicon-other.svg'
+import { useEffect, useState } from "react"
 
 const Hero = () => {
 
-    const { t } = useTranslation();
+    const { t } = useTranslation()
+
+    const heroIcons = [html, css, javascript, typescrip, react, next, redux, sass, gulp, other]
+
+    const showIconEffect = `${styles.showIcon} fadeIn2`
+
+    const [activeClass, setActiveClass] = useState(
+        Array(heroIcons.length).fill(styles.hideIcon)
+    )
+
+    useEffect(() => {
+        let index = 0;
+        const interval = setInterval(() => {
+            if (index < heroIcons.length) {
+                setActiveClass((prev) =>
+                    prev.map((className, i) => (i === index ? showIconEffect : className))
+                );
+                index++;
+            } else {
+                clearInterval(interval);
+            }
+        }, 200);
+    
+        return () => clearInterval(interval);
+    }, [showIconEffect]);
+
+    useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+        if (index < heroIcons.length) {
+            setActiveClass((prev) =>
+                prev.map((className, i) => (i === index ? showIconEffect : className))
+            );
+            index++;
+        } else {
+            clearInterval(interval);
+        }
+    }, 200);
+
+    return () => clearInterval(interval);
+}, [showIconEffect]);
 
     return (
         <>
             <section className={styles.hero} id="main">
                 <div className={styles.mainLayer}>
-                <img src={vfo} className={styles.vfoLogo} alt="VFO logo" />
+                    <img src={vfo} className={styles.vfoLogo} alt="VFO logo" />
                     <h1 className="fadeIn">Victor Freire Oliveira
                         <br /><span className={`${styles.subtitle} fadeIn2`}>{t('VFOsubtitle')}</span>
                     </h1>
-                    <div className={`${styles.heroIcons} fadeIn3`}>
-                        <img src={html} alt="HTML icon" />
-                        <img src={css} alt="CSS icon" />
-                        <img src={javascript} alt="JavaScript icon" />
-                        <img src={typescrip} alt="TypeScript icon" />
-                        <img src={react} alt="React icon" />
-                        <img src={next} alt="Next icon" />
-                        <img src={redux} alt="Redux icon" />
-                        <img src={sass} alt="Sass icon" />
-                        <img src={gulp} alt="Gulp icon" />
-                        <img src={other} alt="Other icon" />
+                    <div className={`${styles.heroIcons}`}>
+                        {heroIcons.map((icon, index) => (
+                            <img src={icon} key={index} className={activeClass[index]} countalt="Tech icon" />
+                        ))}
                     </div>
                     <a href="#services">
                         <button className={`${styles.startBtn} ${styles.btn1}`}>{t('startBtn')}
